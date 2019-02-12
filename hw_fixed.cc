2d75941b429250ce904c52a539701323164a97ef
James <jamie.a.fox@gmail.com>
2019-02-12 02:57:26 -0500

    Innitial Commit - Corrected Std Deviation Function - Doxygen Comments

---
+++ hw_fixed.cc
@@ -0,0 +1,40 @@
+/**
+* \author James Fox
+* \date February 11, 2019
+* \file hw_fixed.cc
+*/
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+
+/*!
+*   \brief Function finds the standard deviation within a given array of numbers
+*   \param a Pointer to the first element of an array
+    \param n Size of array a
+*   \return Returns a double standard deviation
+*/
+double deviation( int* a, int n )
+{
+    int* v = a;
+        double sum;
+        for(size_t i = 0; i <= n-1; i++)
+        {
+                sum += a[i];
+        }
+
+        double mean = sum / n;
+        double stddev = 0;
+        for(size_t i = 0; i <= n -1; i++)
+        {
+                stddev = stddev + (v[i] - mean) * (v[i] - mean);
+        }
+        //std::cout << "E stdev = " << stddev << std::endl;
+        stddev /= n;
+    //std::cout << "E stdev / n = " << stddev << std::endl;
+
+        if( stddev == 0)
+                std::cout << "Sigma is zero." << std::endl;
+        return sqrt(stddev);
+}
